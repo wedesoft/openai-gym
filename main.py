2318c8cde3dea81717871492bb01e31449af391e
Jan Wedekind (Dr) <jan@wedesoft.de>
2020-01-31 10:11:16 +0000

    Run multiple episodes

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import time
 import gym
 env = gym.make('LunarLander-v2')
-state = env.reset()
-print(env.action_space)
-print(env.observation_space)
-print(env.observation_space.low)
-print(env.observation_space.high)
-done = False
-while not done:
-    env.render()
-    action = env.action_space.sample()
-    state, reward, done, _ = env.step(action)
-    print(state, reward, done)
-    time.sleep(0.04)
+for e in range(10):
+    done = False
+    state = env.reset()
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        state, reward, done, _ = env.step(action)
 env.close()
